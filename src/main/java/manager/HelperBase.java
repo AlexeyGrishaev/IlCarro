5feaa1e953bf0ae20b624dbd2cf536037acdca43
Maria Z <z0559882272@gmail.com>
2024-06-06 19:39:35 +0300

    search Any Period

--- src/main/java/manager/HelperBase.java
+++ src/main/java/manager/HelperBase.java
@@ -1,9 +1,10 @@
 package manager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import com.google.common.io.Files;
+import org.openqa.selenium.*;
+
+import java.io.File;
+import java.io.IOException;
 
 public class HelperBase {
     WebDriver wd;
@@ -17,17 +18,17 @@ public class HelperBase {
     }
 
 
-    public void type(By locator, String text){
+    public void type(By locator, String text) {
         WebElement element = wd.findElement(locator);
         element.click();
         element.clear();
         clearNew(element);
-        if(text!=null){
+        if (text != null) {
             element.sendKeys(text);
         }
     }
 
-    public void clearNew(WebElement element){
+    public void clearNew(WebElement element) {
         element.sendKeys(" ");
         element.sendKeys(Keys.BACK_SPACE);
 
@@ -47,7 +48,7 @@ public class HelperBase {
         click(By.xpath("//button[@type='submit']"));
     }
 
-    public void pause(int time){
+    public void pause(int time) {
         try {
             Thread.sleep(time);
         } catch (InterruptedException e) {
@@ -56,6 +57,28 @@ public class HelperBase {
     }
 
     public boolean isElementPresent(By locator) {
-        return wd.findElements(locator).size()>0;
+        return wd.findElements(locator).size() > 0;
+    }
+
+    public void getScreen(String link) {
+        TakesScreenshot takesScreenshot = (TakesScreenshot) wd;
+        File tmp = takesScreenshot.getScreenshotAs(OutputType.FILE);
+        try {
+            Files.copy(tmp, new File(link));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void clearTextBox(By locator) {
+        WebElement element = wd.findElement(locator);
+        String os = System.getProperty("os.name");
+        if (os.startsWith("Win")) {
+            element.sendKeys(Keys.CONTROL, "a");
+        } else {
+            element.sendKeys(Keys.COMMAND, "a");
+        }
+        element.sendKeys(Keys.DELETE);
+
     }
 }
